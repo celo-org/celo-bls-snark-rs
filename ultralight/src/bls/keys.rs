6f5aab85759b8026294644f69a651cbf46743ed7
Kobi Gurkan <kobigurk@gmail.com>
2019-06-27 18:43:39 +0300

    Adds a Go module consuming bls-zexe and a usage example (#10)
    * feat: adds go module and a usage example
    
    * feat: adds circleci config for go module
    
    * fix: fixes aliases in circleci config
    
    * fixes: gives checkout-repo an executor and build lib
    
    * fix: adds both crate types for build
    
    * fix(ci): changes go docker image to correct one
    
    * fix(ci): makes cd in go tests use an absolute path
    
    * fix(ci): tries fixing workspace location
    
    * fix(ci): attemps persisting the workspace
    
    * fix(ci): puts correct workspace relative path
    
    * feat: adds test for go module

--- ultralight/src/bls/keys.rs
+++ ultralight/src/bls/keys.rs
@@ -1,7 +1,10 @@
 use crate::curve::hash::HashToG2;
 use algebra::{
+    bytes::{FromBytes, ToBytes},
     curves::{
-        bls12_377::{Bls12_377, Bls12_377Parameters, G1Affine, G1Projective, G2Projective},
+        bls12_377::{
+            Bls12_377, Bls12_377Parameters, G1Affine, G1Projective, G2Affine, G2Projective,
+        },
         AffineCurve, PairingCurve, PairingEngine, ProjectiveCurve,
     },
     fields::{
@@ -9,11 +12,15 @@ use algebra::{
         Field,
     },
 };
+
 use failure::Error;
 use rand::Rng;
 
 /// Implements BLS signatures as specified in https://crypto.stanford.edu/~dabo/pubs/papers/BLSmultisig.html.
-use std::ops::{Mul, Neg};
+use std::{
+    io::{Read, Result as IoResult, Write},
+    ops::{Mul, Neg},
+};
 
 pub struct PrivateKey {
     sk: Fr,
@@ -42,6 +49,21 @@ impl PrivateKey {
 
     pub fn to_public(&self) -> PublicKey {
         PublicKey::from_pk(&G1Projective::prime_subgroup_generator().mul(&self.sk))
+    }
+}
+
+impl ToBytes for PrivateKey {
+    #[inline]
+    fn write<W: Write>(&self, mut writer: W) -> IoResult<()> {
+        self.sk.write(&mut writer)
+    }
+}
+
+impl FromBytes for PrivateKey {
+    #[inline]
+    fn read<R: Read>(mut reader: R) -> IoResult<Self> {
+        let sk = Fr::read(&mut reader)?;
+        Ok(PrivateKey::from_sk(&sk))
     }
 }
 
@@ -100,6 +122,21 @@ impl PublicKey {
     }
 }
 
+impl ToBytes for PublicKey {
+    #[inline]
+    fn write<W: Write>(&self, mut writer: W) -> IoResult<()> {
+        self.pk.into_affine().write(&mut writer)
+    }
+}
+
+impl FromBytes for PublicKey {
+    #[inline]
+    fn read<R: Read>(mut reader: R) -> IoResult<Self> {
+        let pk = G1Affine::read(&mut reader)?;
+        Ok(PublicKey::from_pk(&pk.into_projective()))
+    }
+}
+
 pub struct Signature {
     sig: G2Projective,
 }
@@ -120,6 +157,21 @@ impl Signature {
         }
 
         Signature { sig: asig }
+    }
+}
+
+impl ToBytes for Signature {
+    #[inline]
+    fn write<W: Write>(&self, mut writer: W) -> IoResult<()> {
+        self.sig.into_affine().write(&mut writer)
+    }
+}
+
+impl FromBytes for Signature {
+    #[inline]
+    fn read<R: Read>(mut reader: R) -> IoResult<Self> {
+        let sig = G2Affine::read(&mut reader)?;
+        Ok(Signature::from_sig(&sig.into_projective()))
     }
 }
 
