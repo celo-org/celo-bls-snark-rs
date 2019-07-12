a1de980e84bfe480c7af6d75c7d73fa011ba7fc6
Kobi Gurkan <kobigurk@gmail.com>
2019-07-12 22:52:34 +0300

    feat: adds proof of possession and a BLS sig without pedersen hashes (#24)
    * feat: adds proof of possession and a BLS sig without pedersen hashes
    
    * feat: adds proof-of-possession citation

--- go/bls_test.go
+++ go/bls_test.go
@@ -10,8 +10,8 @@ func TestAggregatedSig(t *testing.T) {
 	defer privateKey.Destroy()
 	publicKey, _ := privateKey.ToPublic()
 	message := []byte("test")
-	signature, _ := privateKey.SignMessage(message)
-	err := publicKey.VerifySignature(message, signature)
+	signature, _ := privateKey.SignMessage(message, true)
+	err := publicKey.VerifySignature(message, signature, true)
 	if err != nil {
 		t.Fatalf("failed verifying signature for pk 1, error was: %s", err)
 	}
@@ -19,21 +19,77 @@ func TestAggregatedSig(t *testing.T) {
 	privateKey2, _ := GeneratePrivateKey()
 	defer privateKey2.Destroy()
 	publicKey2, _ := privateKey2.ToPublic()
-	signature2, _ := privateKey2.SignMessage(message)
-	err = publicKey2.VerifySignature(message, signature2)
+	signature2, _ := privateKey2.SignMessage(message, true)
+	err = publicKey2.VerifySignature(message, signature2, true)
 	if err != nil {
 		t.Fatalf("failed verifying signature for pk 2, error was: %s", err)
 	}
 
 	aggergatedPublicKey, _ := AggregatePublicKeys([]*PublicKey{publicKey, publicKey2})
 	aggergatedSignature, _ := AggregateSignatures([]*Signature{signature, signature2})
-	err = aggergatedPublicKey.VerifySignature(message, aggergatedSignature)
+	err = aggergatedPublicKey.VerifySignature(message, aggergatedSignature, true)
 	if err != nil {
 		t.Fatalf("failed verifying signature for aggregated pk, error was: %s", err)
 	}
-	err = publicKey.VerifySignature(message, aggergatedSignature)
+	err = publicKey.VerifySignature(message, aggergatedSignature, true)
 	if err == nil {
-		t.Fatalf("succeed verifying signature for wrong pk, shouldn't have!")
+		t.Fatalf("succeeded verifying signature for wrong pk, shouldn't have!")
+	}
+
+}
+
+func TestProofOfPossession(t *testing.T) {
+	privateKey, _ := GeneratePrivateKey()
+	defer privateKey.Destroy()
+	publicKey, _ := privateKey.ToPublic()
+	signature, _ := privateKey.SignPoP()
+	err := publicKey.VerifyPoP(signature)
+	if err != nil {
+		t.Fatalf("failed verifying PoP for pk 1, error was: %s", err)
+	}
+
+	privateKey2, _ := GeneratePrivateKey()
+	defer privateKey2.Destroy()
+	publicKey2, _ := privateKey2.ToPublic()
+	if err != nil {
+		t.Fatalf("failed verifying PoP for pk 2, error was: %s", err)
+	}
+
+	err = publicKey2.VerifyPoP(signature)
+	if err == nil {
+		t.Fatalf("succeeded verifying PoP for wrong pk, shouldn't have!")
+	}
+}
+
+func TestNonCompositeSig(t *testing.T) {
+	privateKey, _ := GeneratePrivateKey()
+	defer privateKey.Destroy()
+	publicKey, _ := privateKey.ToPublic()
+	message := []byte("test")
+	signature, _ := privateKey.SignMessage(message, false)
+	err := publicKey.VerifySignature(message, signature, false)
+	if err != nil {
+		t.Fatalf("failed verifying signature for pk 1, error was: %s", err)
+	}
+
+	privateKey2, _ := GeneratePrivateKey()
+	defer privateKey2.Destroy()
+	publicKey2, _ := privateKey2.ToPublic()
+	signature2, _ := privateKey2.SignMessage(message, false)
+	err = publicKey2.VerifySignature(message, signature2, false)
+	if err != nil {
+		t.Fatalf("failed verifying signature for pk 2, error was: %s", err)
+	}
+
+	aggergatedPublicKey, _ := AggregatePublicKeys([]*PublicKey{publicKey, publicKey2})
+	aggergatedSignature, _ := AggregateSignatures([]*Signature{signature, signature2})
+	err = aggergatedPublicKey.VerifySignature(message, aggergatedSignature, false)
+	if err != nil {
+		t.Fatalf("failed verifying signature for aggregated pk, error was: %s", err)
+	}
+	err = publicKey.VerifySignature(message, aggergatedSignature, false)
+	if err == nil {
+		t.Fatalf("succeeded verifying signature for wrong pk, shouldn't have!")
 	}
 
 }
