2a10b4ae558208e84eac24d1a49b7d298e8b0730
Jason Ansel <jansel@jansel.net>
2020-02-22 10:43:56 -0800

    Add caching for public key processing

--- bls/algebra/src/bls/keys.rs
+++ bls/algebra/src/bls/keys.rs
@@ -1,4 +1,13 @@
+extern crate lru;
+
+use lru::LruCache;
+use lazy_static::lazy_static;
+use std::sync::Mutex;
+use std::hash::{Hash, Hasher};
+use std::collections::HashSet;
+
 use crate::curve::hash::{HashToG1};
+
 use algebra::{
     bytes::{
         FromBytes,
@@ -106,8 +115,22 @@ impl Error for BLSError {
     }
 }
 
+#[derive(Eq)]
 pub struct PublicKey {
     pk: G2Projective,
+}
+
+struct AggregateCacheState {
+    keys : HashSet<PublicKey>,
+    combined : G2Projective,
+}
+
+lazy_static! {
+    static ref FROM_BYTES_CACHE: Mutex<LruCache<Vec<u8>, PublicKey>> = Mutex::new(LruCache::new(128));
+    static ref AGGREGATE_CACHE: Mutex<AggregateCacheState> = Mutex::new(AggregateCacheState{
+        keys: HashSet::new(),
+        combined: G2Projective::zero().clone()
+    });
 }
 
 impl PublicKey {
@@ -119,90 +142,37 @@ impl PublicKey {
         self.pk.clone()
     }
 
-    pub fn aggregate(public_keys: &[&PublicKey]) -> PublicKey {
-        let mut apk = G2Projective::zero();
-        for i in public_keys.iter() {
-            apk = apk + &(*i).pk;
-        }
-
-        PublicKey { pk: apk }
+    pub fn clone(&self) -> PublicKey {
+        PublicKey::from_pk(&self.pk)
     }
 
-    pub fn verify<H: HashToG1>(
-        &self,
-        message: &[u8],
-        extra_data: &[u8],
-        signature: &Signature,
-        hash_to_g1: &H,
-    ) -> Result<(), Box<dyn Error>> {
-        self.verify_sig(SIG_DOMAIN, message, extra_data, signature, hash_to_g1)
+    pub fn clear_cache() {
+        FROM_BYTES_CACHE.lock().unwrap().clear();
+        let mut cache = AGGREGATE_CACHE.lock().unwrap();
+        cache.keys = HashSet::new();
+        cache.combined = G2Projective::zero().clone();
     }
 
-    pub fn verify_pop<H: HashToG1>(
-        &self,
-        message: &[u8],
-        signature: &Signature,
-        hash_to_g1: &H,
-    ) -> Result<(), Box<dyn Error>> {
-        self.verify_sig(POP_DOMAIN, &message, &[], signature, hash_to_g1)
-    }
-
-
-    fn verify_sig<H: HashToG1>(
-        &self,
-        domain: &[u8],
-        message: &[u8],
-        extra_data: &[u8],
-        signature: &Signature,
-        hash_to_g1: &H,
-    ) -> Result<(), Box<dyn Error>> {
-        let pairing = Bls12_377::product_of_pairings(&vec![
-            (
-                &signature.get_sig().into_affine().prepare(),
-                &G2Affine::prime_subgroup_generator().neg().prepare(),
-            ),
-            (
-                &hash_to_g1
-                    .hash::<Bls12_377Parameters>(domain, message, extra_data)?
-                    .into_affine()
-                    .prepare(),
-                &self.pk.into_affine().prepare(),
-            ),
-        ]);
-        if pairing == Fq12::one() {
-            Ok(())
+    pub fn from_bytes(data: &Vec<u8>) -> IoResult<PublicKey> {
+        let cached_result = PublicKey::from_bytes_cached(data);
+        if cached_result.is_none() {
+            // cache miss
+            let generated_result = PublicKey::from_bytes_uncached(data)?;
+            FROM_BYTES_CACHE.lock().unwrap().put(data.to_owned(), generated_result.clone());
+            Ok(generated_result)
         } else {
-            Err(BLSError::VerificationFailed)?
+            // cache hit
+            Ok(cached_result.unwrap())
         }
     }
-}
 
-impl ToBytes for PublicKey {
-    #[inline]
-    fn write<W: Write>(&self, mut writer: W) -> IoResult<()> {
-        let affine = self.pk.into_affine();
-        let mut x_bytes: Vec<u8> = vec![];
-        let y_c0_big = affine.y.c0.into_repr();
-        let y_c1_big = affine.y.c1.into_repr();
-        let half = Fq::modulus_minus_one_div_two();
-        affine.x.write(&mut x_bytes)?;
-        let num_x_bytes = x_bytes.len();
-        if y_c1_big > half {
-            x_bytes[num_x_bytes - 1] |= 0x80;
-        } else if y_c1_big == half && y_c0_big > half {
-            x_bytes[num_x_bytes - 1] |= 0x80;
-        }
-        writer.write(&x_bytes)?;
-
-        Ok(())
+    pub fn from_bytes_cached(data: &Vec<u8>) -> Option<PublicKey> {
+        let mut cache = FROM_BYTES_CACHE.lock().unwrap();
+        Some(cache.get(data)?.clone())
     }
-}
 
-impl FromBytes for PublicKey {
-    #[inline]
-    fn read<R: Read>(mut reader: R) -> IoResult<Self> {
-        let mut x_bytes_with_y: Vec<u8> = vec![];
-        reader.read_to_end(&mut x_bytes_with_y)?;
+    pub fn from_bytes_uncached(data: &Vec<u8>) -> IoResult<PublicKey> {
+        let mut x_bytes_with_y: Vec<u8> = data.to_owned();
         let x_bytes_with_y_len = x_bytes_with_y.len();
         let y_over_half = (x_bytes_with_y[x_bytes_with_y_len - 1] & 0x80) == 0x80;
         x_bytes_with_y[x_bytes_with_y_len - 1] &= 0xFF - 0x80;
@@ -233,7 +203,122 @@ impl FromBytes for PublicKey {
         let chosen_y = if y_over_half { bigger } else { smaller };
         let pk = G2Affine::new(x, chosen_y, false);
         Ok(PublicKey::from_pk(&pk.into_projective()))
+    }
 
+    pub fn aggregate(public_keys: &[&PublicKey]) -> PublicKey {
+        // The set of validators changes slowly, so for speed we will compute the
+        // difference from the last call and do an incremental update
+        let mut keys : HashSet<PublicKey> = HashSet::with_capacity(public_keys.len());
+        for key in public_keys.iter() {
+            keys.insert((*key).clone());
+        }
+        let mut cache = AGGREGATE_CACHE.lock().unwrap();
+        let mut combined = cache.combined;
+
+        for key in cache.keys.difference(&keys) {
+            combined = combined - &(*key).pk;
+        }
+
+        for key in keys.difference(&cache.keys) {
+            combined = combined + &(*key).pk;
+        }
+
+        cache.keys = keys;
+        cache.combined = combined;
+        PublicKey::from_pk(&combined)
+    }
+
+    pub fn verify<H: HashToG1>(
+        &self,
+        message: &[u8],
+        extra_data: &[u8],
+        signature: &Signature,
+        hash_to_g1: &H,
+    ) -> Result<(), Box<dyn Error>> {
+        self.verify_sig(SIG_DOMAIN, message, extra_data, signature, hash_to_g1)
+    }
+
+    pub fn verify_pop<H: HashToG1>(
+        &self,
+        message: &[u8],
+        signature: &Signature,
+        hash_to_g1: &H,
+    ) -> Result<(), Box<dyn Error>> {
+        self.verify_sig(POP_DOMAIN, &message, &[], signature, hash_to_g1)
+    }
+
+    fn verify_sig<H: HashToG1>(
+        &self,
+        domain: &[u8],
+        message: &[u8],
+        extra_data: &[u8],
+        signature: &Signature,
+        hash_to_g1: &H,
+    ) -> Result<(), Box<dyn Error>> {
+        let pairing = Bls12_377::product_of_pairings(&vec![
+            (
+                &signature.get_sig().into_affine().prepare(),
+                &G2Affine::prime_subgroup_generator().neg().prepare(),
+            ),
+            (
+                &hash_to_g1
+                    .hash::<Bls12_377Parameters>(domain, message, extra_data)?
+                    .into_affine()
+                    .prepare(),
+                &self.pk.into_affine().prepare(),
+            ),
+        ]);
+        if pairing == Fq12::one() {
+            Ok(())
+        } else {
+            Err(BLSError::VerificationFailed)?
+        }
+    }
+}
+
+impl PartialEq for PublicKey {
+    fn eq(&self, other: &Self) -> bool {
+        // The equality operator in G2Projective is very slow
+        self.pk.x == other.pk.x && self.pk.y == other.pk.y && self.pk.z == other.pk.z
+    }
+}
+
+impl Hash for PublicKey {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        // Only hash based on `y` for slight speed improvement
+        self.pk.y.hash(state);
+        // self.pk.x.hash(state);
+        // self.pk.z.hash(state);
+    }
+}
+
+impl ToBytes for PublicKey {
+    #[inline]
+    fn write<W: Write>(&self, mut writer: W) -> IoResult<()> {
+        let affine = self.pk.into_affine();
+        let mut x_bytes: Vec<u8> = vec![];
+        let y_c0_big = affine.y.c0.into_repr();
+        let y_c1_big = affine.y.c1.into_repr();
+        let half = Fq::modulus_minus_one_div_two();
+        affine.x.write(&mut x_bytes)?;
+        let num_x_bytes = x_bytes.len();
+        if y_c1_big > half {
+            x_bytes[num_x_bytes - 1] |= 0x80;
+        } else if y_c1_big == half && y_c0_big > half {
+            x_bytes[num_x_bytes - 1] |= 0x80;
+        }
+        writer.write(&x_bytes)?;
+
+        Ok(())
+    }
+}
+
+impl FromBytes for PublicKey {
+    #[inline]
+    fn read<R: Read>(mut reader: R) -> IoResult<Self> {
+        let mut x_bytes_with_y: Vec<u8> = vec![];
+        reader.read_to_end(&mut x_bytes_with_y)?;
+        PublicKey::from_bytes(&x_bytes_with_y)
     }
 }
 
@@ -362,7 +447,7 @@ mod test {
         }
     }
 
-        #[test]
+    #[test]
     fn test_pop() {
         init();
 
