a81bcc94d2da47fbda7b300d7492c660c2937abd
Georgios Konstantopoulos <me@gakonst.com>
2020-03-08 16:20:24 +0200

    feat(gadget/bitmap): generalize to counting both 1s and 0s

--- bls/gadgets/src/bitmap.rs
+++ bls/gadgets/src/bitmap.rs
@@ -7,52 +7,61 @@ use r1cs_std::{
     Assignment,
 };
 
-/// Enforces that there are no more than `max_zeros` present in the provided bitmap
-pub fn enforce_maximum_zeros_in_bitmap<F: PrimeField, CS: ConstraintSystem<F>>(
+/// Enforces that there are no more than `max_occurrences` of `value` (0 or 1)
+/// present in the provided bitmap
+pub fn enforce_maximum_occurrences_in_bitmap<F: PrimeField, CS: ConstraintSystem<F>>(
     cs: &mut CS,
     bitmap: &[Boolean],
-    max_zeros: u64,
+    max_occurrences: u64,
+    value: bool,
 ) -> Result<(), SynthesisError> {
+    let mut value_fp = F::one();
+    if !value {
+        value_fp = value_fp.neg();
+    }
     // If we're in setup mode, we skip the bit counting part since the bitmap
     // will be empty
     let is_setup = bitmap.iter().all(|bit| bit.get_value().is_none());
 
-    // Calculate the number of zeros
-    let mut num_zeros = 0;
-    let mut num_zeros_lc = LinearCombination::zero();
-    // For each bit, increment the number of zeros
-    // if the bit was a 0. We calculate both the number of zeros
+    let mut occurrences = 0;
+    let mut occurrences_lc = LinearCombination::zero();
+    // For each bit, increment the number of occurences if the bit matched `value`
+    // We calculate both the number of occurrences
     // and a linear combination over it, in order to do 2 things:
-    // 1. enforce that num_zeros < maximum_zeros
-    // 2. enforce that num_zeros was calculated correctly from the bitmap
+    // 1. enforce that occurrences < maximum_occurences
+    // 2. enforce that occurrences was calculated correctly from the bitmap
     for bit in bitmap {
         // Update the constraints
-        num_zeros_lc += (F::one(), CS::one());
-        let zero_lc = bit.lc(CS::one(), F::one().neg());
-        num_zeros_lc = num_zeros_lc + zero_lc;
+        if !value {
+            // add 1 here only for zeros
+            occurrences_lc += (F::one(), CS::one());
+        }
+        occurrences_lc = occurrences_lc + bit.lc(CS::one(), value_fp);
 
         // Update our count
         if !is_setup {
-            let is_zero = !bit.get_value().get()?;
-            num_zeros += is_zero as u8;
+            let got_value = bit.get_value().get()?;
+            occurrences += (got_value == value) as u8;
         }
     }
-    // Rebind `num_zeros` to a constraint
-    let num_zeros = FpGadget::alloc(&mut cs.ns(|| "num zeros"), || Ok(F::from(num_zeros)))?;
+    // Rebind `occurrences` to a constraint
+    let occurrences = FpGadget::alloc(&mut cs.ns(|| "num occurrences"), || {
+        Ok(F::from(occurrences))
+    })?;
 
-    let num_zeros_bits = &num_zeros.to_bits(&mut cs.ns(|| "num zeros to bits"))?;
+    let occurrences_bits = &occurrences.to_bits(&mut cs.ns(|| "num occurrences to bits"))?;
     Boolean::enforce_smaller_or_equal_than::<_, _, F, _>(
-        &mut cs.ns(|| "enforce maximum number of zeros"),
-        num_zeros_bits,
-        F::from(max_zeros).into_repr(),
+        &mut cs.ns(|| "enforce maximum number of occurrences"),
+        occurrences_bits,
+        F::from(max_occurrences).into_repr(),
     )?;
 
-    // Enforce that we have correctly counted the number of zeros
+    // Enforce that we have correctly counted the number of occurrences
     cs.enforce(
-        || "enforce num zeros lc equal to num",
-        |_| num_zeros_lc,
+        || "enforce num occurrences lc equal to num",
+        |_| occurrences_lc,
         |lc| lc + (F::one(), CS::one()),
-        |lc| num_zeros.get_variable() + lc,
+        |lc| occurrences.get_variable() + lc,
     );
 
     Ok(())
@@ -64,33 +73,67 @@ mod tests {
     use algebra::bls12_377::Fq;
     use r1cs_std::test_constraint_system::TestConstraintSystem;
 
-    fn cs_enforce(bitmap: &[bool], max_zeros: u64) -> TestConstraintSystem<Fq> {
+    fn cs_enforce_value(
+        bitmap: &[bool],
+        max_number: u64,
+        is_one: bool,
+    ) -> TestConstraintSystem<Fq> {
         let mut cs = TestConstraintSystem::<Fq>::new();
         let bitmap = bitmap
             .into_iter()
             .map(|b| Boolean::constant(*b))
             .collect::<Vec<_>>();
-        enforce_maximum_zeros_in_bitmap(&mut cs, &bitmap, max_zeros).unwrap();
+        enforce_maximum_occurrences_in_bitmap(&mut cs, &bitmap, max_number, is_one).unwrap();
         cs
     }
 
-    #[test]
-    fn one() {
-        assert!(cs_enforce(&[true], 0).is_satisfied());
+    mod zeros {
+        use super::*;
+
+        #[test]
+        fn one_zero_allowed() {
+            assert!(cs_enforce_value(&[false], 1, false).is_satisfied());
+        }
+
+        #[test]
+        fn no_zeros_allowed() {
+            assert!(!cs_enforce_value(&[false], 0, false).is_satisfied());
+        }
+
+        #[test]
+        fn three_zeros_allowed() {
+            assert!(cs_enforce_value(&[false, true, true, false, false], 3, false).is_satisfied());
+        }
+
+        #[test]
+        fn four_zeros_not_allowed() {
+            assert!(
+                !cs_enforce_value(&[false, false, true, false, false], 3, false).is_satisfied()
+            );
+        }
     }
 
-    #[test]
-    fn no_zeros_allowed() {
-        assert!(!cs_enforce(&[false], 0).is_satisfied());
-    }
+    mod ones {
+        use super::*;
 
-    #[test]
-    fn three_zeros_allowed() {
-        assert!(cs_enforce(&[false, true, true, false, false], 3).is_satisfied());
-    }
+        #[test]
+        fn one_one_allowed() {
+            assert!(cs_enforce_value(&[true], 1, true).is_satisfied());
+        }
 
-    #[test]
-    fn four_zeros_not_allowed() {
-        assert!(!cs_enforce(&[false, false, true, false, false], 3).is_satisfied());
+        #[test]
+        fn no_ones_allowed() {
+            assert!(!cs_enforce_value(&[true], 0, true).is_satisfied());
+        }
+
+        #[test]
+        fn three_ones_allowed() {
+            assert!(cs_enforce_value(&[false, true, true, true, false], 3, true).is_satisfied());
+        }
+
+        #[test]
+        fn four_ones_not_allowed() {
+            assert!(!cs_enforce_value(&[true, true, true, true, false], 3, true).is_satisfied());
+        }
     }
 }
--- bls/gadgets/src/lib.rs
+++ bls/gadgets/src/lib.rs
@@ -4,10 +4,13 @@ mod bls;
 pub use bls::BlsVerifyGadget;
 
 mod bitmap;
-pub use bitmap::enforce_maximum_zeros_in_bitmap;
+pub use bitmap::enforce_maximum_occurrences_in_bitmap;
 
 mod y_to_bit;
 pub use y_to_bit::YToBitGadget;
 
 mod hash_to_group;
 pub use hash_to_group::HashToGroupGadget;
+
+mod validator;
+pub use validator::ValidatorUpdateGadget;
