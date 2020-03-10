75711a9260d0edf4450c8ceb05245da86d6c24cc
Kobi Gurkan <kobigurk@gmail.com>
2020-03-10 18:32:53 +0200

    removes bls_crypto import, since epoch_snark uses it

--- go/bls_arm7.go
+++ go/bls_arm7.go
@@ -3,6 +3,6 @@
 package bls
 
 /*
-#cgo LDFLAGS: -L../target/arm-unknown-linux-gnueabihf/release -lbls_crypto -lepoch_snark -ldl -lm
+#cgo LDFLAGS: -L../target/arm-unknown-linux-gnueabihf/release -lepoch_snark -ldl -lm
 */
 import "C"
--- go/bls_windows64.go
+++ go/bls_windows64.go
@@ -3,6 +3,6 @@
 package bls
 
 /*
-#cgo LDFLAGS: -L../target/x86_64-pc-windows-gnu/release -lbls_crypto -lepoch_snark -lm -lws2_32 -luserenv -lunwind
+#cgo LDFLAGS: -L../target/x86_64-pc-windows-gnu/release -lepoch_snark -lm -lws2_32 -luserenv -lunwind
 */
 import "C"
