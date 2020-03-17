2ba991c738cfa336922ce8f9cc3fd9d51ab9e6ed
Kobi Gurkan <kobigurk@gmail.com>
2020-03-17 23:14:17 +0200

    fixes paste

--- go/bls/bls_linux386.go
+++ go/bls/bls_linux386.go
@@ -6,4 +6,3 @@ package bls
 #cgo LDFLAGS: -L../../target/i686-unknown-linux-gnu/release -lepoch_snark -ldl -lm
 */
 import "C"
-<Paste>
--- go/snark/snark_linux386.go
+++ go/snark/snark_linux386.go
@@ -6,4 +6,3 @@ package snark
 #cgo LDFLAGS: -L../../target/i686-unknown-linux-gnu/release -lepoch_snark -ldl -lm
 */
 import "C"
-<Paste>
