dcce16b4ef55d3cc5f08cc6ff6e2479de46fafb6
Georgios Konstantopoulos <me@gakonst.com>
2020-03-26 08:52:50 +0200

    examples: calculate number of constraints from empty circuit (#126)
    * examples: calculate number of constraints from empty circuit
    
    * chore: use correct method for to bits conversion
    
    * fix(gadgets/smaller_than): use safe to_bits method
    
    * chore: switch back to latest Zexe Master

--- crates/epoch-snark/examples/constraints.rs
+++ crates/epoch-snark/examples/constraints.rs
@@ -1,19 +1,9 @@
-use algebra::{bls12_377::G1Projective, Zero};
-use epoch_snark::{
-    api::{prover, BLSCurve, CPFrParams},
-    gadgets::{HashToBits, ValidatorSetUpdate},
-};
-use groth16::generate_random_parameters;
+use epoch_snark::{api::BLSCurve, gadgets::ValidatorSetUpdate};
 use r1cs_core::ConstraintSynthesizer;
-use r1cs_std::test_constraint_system::TestConstraintSystem;
+use r1cs_std::test_constraint_counter::ConstraintCounter;
 use std::env;
 
-#[path = "../tests/fixtures.rs"]
-mod fixtures;
-use fixtures::generate_test_data;
-
 fn main() {
-    let rng = &mut rand::thread_rng();
     let mut args = env::args();
     args.next().unwrap(); // discard the program name
     let num_validators = args
@@ -26,50 +16,17 @@ fn main() {
         .expect("num epochs was expected")
         .parse()
         .expect("NaN");
-    let hashes_in_bls12_377: bool = args
-        .next()
-        .expect("expected flag for generating or not constraints inside BLS12_377")
-        .parse()
-        .expect("not a bool");
     let faults = (num_validators - 1) / 3;
 
-    // Make a random initialization of the circuit
-    let (first_epoch, transitions, _) = generate_test_data(num_validators, faults, num_epochs);
-
-    // Trusted setup for the HashToBits circuit if required
-    let hash_helper = if hashes_in_bls12_377 {
-        let empty_hash_to_bits = HashToBits::empty::<CPFrParams>(num_epochs);
-        let params = generate_random_parameters(empty_hash_to_bits, rng).unwrap();
-        let helper = prover::generate_hash_helper(&params, &transitions).unwrap();
-        Some(helper)
-    } else {
-        None
-    };
-
-    let asig = transitions.iter().fold(G1Projective::zero(), |acc, epoch| {
-        acc + epoch.aggregate_signature.get_sig()
-    });
-    let epochs = transitions
-        .iter()
-        .map(|transition| prover::to_update(transition))
-        .collect::<Vec<_>>();
-    let circuit = ValidatorSetUpdate::<BLSCurve> {
-        initial_epoch: prover::to_epoch_data(&first_epoch),
-        epochs,
-        aggregated_signature: Some(asig),
-        num_validators: num_validators as u32,
-        hash_helper,
-    };
-
-    // generate test constraints and log them
-    let mut cs = TestConstraintSystem::new();
+    let mut cs = ConstraintCounter::new();
+    let circuit = ValidatorSetUpdate::<BLSCurve>::empty(num_validators, num_epochs, faults, None);
     circuit.generate_constraints(&mut cs).unwrap();
 
     println!(
-        "Number of constraints for {} epochs ({} validators, hashes in BLS12-377 {}): {}",
+        "Number of constraints for {} epochs ({} validators, {} faults, hashes in SW6): {}",
         num_epochs,
         num_validators,
-        hashes_in_bls12_377,
+        faults,
         cs.num_constraints()
     )
 }
