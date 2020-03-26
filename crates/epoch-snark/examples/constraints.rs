4bd2a6800666fd0b00c05bfe4cd2d0145af5989a
Georgios Konstantopoulos <me@gakonst.com>
2020-03-26 08:29:03 +0200

    chore: switch back to latest Zexe Master

--- crates/epoch-snark/examples/constraints.rs
+++ crates/epoch-snark/examples/constraints.rs
@@ -1,6 +1,6 @@
 use epoch_snark::{api::BLSCurve, gadgets::ValidatorSetUpdate};
 use r1cs_core::ConstraintSynthesizer;
-use r1cs_std::test_constraint_counter::TestConstraintCounter;
+use r1cs_std::test_constraint_counter::ConstraintCounter;
 use std::env;
 
 fn main() {
@@ -18,16 +18,15 @@ fn main() {
         .expect("NaN");
     let faults = (num_validators - 1) / 3;
 
-    let mut cs = TestConstraintCounter::new();
+    let mut cs = ConstraintCounter::new();
     let circuit = ValidatorSetUpdate::<BLSCurve>::empty(num_validators, num_epochs, faults, None);
     circuit.generate_constraints(&mut cs).unwrap();
 
     println!(
-        "Number of constraints for {} epochs ({} validators, {} faults, hashes in BLS12-377 {}): {}",
+        "Number of constraints for {} epochs ({} validators, {} faults, hashes in SW6): {}",
         num_epochs,
         num_validators,
         faults,
-        false,
         cs.num_constraints()
     )
 }
