5ee14c43dd824e05d2598e62e291d68ae116978d
Kobi Gurkan <kobigurk@gmail.com>
2020-04-27 17:25:51 +0300

    removes cofactor.rs

--- crates/bls-crypto/src/hash_to_curve/cofactor.rs
+++
@@ -1,8 +0,0 @@
-use algebra::{
-    bls12::{Bls12Parameters, G1Projective},
-    ProjectiveCurve,
-};
-
-pub fn scale_by_cofactor_g1<P: Bls12Parameters>(p: &G1Projective<P>) -> G1Projective<P> {
-    p.into_affine().scale_by_cofactor()
-}
