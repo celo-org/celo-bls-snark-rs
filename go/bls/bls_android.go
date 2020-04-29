8de9a02bc27993a5e9cbb310cb6c1de9dbdf015f
Kobi Gurkan <kobigurk@gmail.com>
2020-04-29 17:17:58 +0300

    new android paths

--- go/bls/bls_android.go
+++ go/bls/bls_android.go
@@ -3,7 +3,7 @@
 package bls
 
 /*
-#cgo LDFLAGS: -lepoch_snark -ldl -lm
+#cgo LDFLAGS: -lepoch_snark_aarch64-linux-android_0.1.0 -lepoch_snark_armv7-linux-androideabi_0.1.0 -lepoch_snark_i686-linux-android_0.1.0 -lepoch_snark_x86_64-linux-android_0.1.0 -ldl -lm
 */
 import "C"
 
