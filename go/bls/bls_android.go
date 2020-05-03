a5a6dabcce6bc6c2f2a283def863205e0332db8a
Kobi Gurkan <kobigurk@gmail.com>
2020-05-03 08:04:27 +0300

    fixes android paths

--- go/bls/bls_android.go
+++ go/bls/bls_android.go
@@ -3,7 +3,7 @@
 package bls
 
 /*
-#cgo LDFLAGS: -L${SRCDIR}/libs/aarch64-linux-android -L${SRCDIR}/libs/armv7-linux-androideabi -L${SRCDIR}/libs/i686-linux-android -L${SRCDIR}/libs/x86_64-linux-android -lepoch_snark -ldl -lm
+#cgo LDFLAGS: -L${SRCDIR}/../libs/aarch64-linux-android -L${SRCDIR}/../libs/armv7-linux-androideabi -L${SRCDIR}/../libs/i686-linux-android -L${SRCDIR}/../libs/x86_64-linux-android -lepoch_snark -ldl -lm
 */
 import "C"
 
