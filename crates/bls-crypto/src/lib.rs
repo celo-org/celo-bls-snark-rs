117caeb6511fe91b06f295d2cbab36d603784394
Georgios Konstantopoulos <me@gakonst.com>
2020-05-06 12:50:04 +0300

    docs(bls-crypto): expand on top level crate docs

--- crates/bls-crypto/src/lib.rs
+++ crates/bls-crypto/src/lib.rs
@@ -1,6 +1,55 @@
 //! # BLS Cryptography
 //!
-//! This crate implements cryptographic operations for BLS signatures
+//! This crate implements cryptographic operations for BLS signatures.
+//!
+//! It supports:
+//! - signing and verifying BLS signatures
+//! - aggregating BLS signatures and public keys
+//! - batch verification of `n` BLS signatures with `n+1` pairings instead of `2n`
+//! - SNARK-friendly hashing utilizing a Pedersen CRH via the `composite` hasher module
+//!
+//! # Example
+//!
+//! ```rust
+//! use bls_crypto::{PrivateKey, PublicKey, Signature};
+//! use bls_crypto::{
+//!     OUT_DOMAIN,
+//!     hash_to_curve::{HashToCurve, try_and_increment::DIRECT_HASH_TO_G1}
+//! };
+//!
+//! let rng = &mut rand::thread_rng();
+//! let message = &b"hello"[..];
+//! let extra_data = &[];
+//! let hasher = &*DIRECT_HASH_TO_G1;
+//!
+//! // generate a few private keys
+//! let keys = (0..10).map(|_| PrivateKey::generate(rng)).collect::<Vec<_>>();
+//!
+//! // sign each message
+//! let sigs = keys
+//!     .iter()
+//!     .map(|key| key.sign(message, extra_data, hasher).unwrap())
+//!     .collect::<Vec<_>>();
+//!
+//! // gets the public keys
+//! let public_keys = keys.iter().map(|key| key.to_public()).collect::<Vec<_>>();
+//!
+//! // Each signature can be verified individually against the key
+//! public_keys.iter().zip(&sigs).for_each(|(pubkey, sig)| {
+//!     pubkey.verify(message, extra_data, &sig, hasher).unwrap();
+//! });
+//!
+//! // The aggregate signature can be verified against the aggregate public key
+//! let aggregate_sig = Signature::aggregate(&sigs);
+//! let aggregate_public_key = PublicKey::aggregate(&public_keys);
+//! aggregate_public_key.verify(message, extra_data, &aggregate_sig, hasher).unwrap();
+//! ```
+//!
+//! # Notes
+//!
+//! Currently the supported curves are BLS12-377 with signatures on G1 and public keys on G2.
+//! In a future iteration, this will be abstracted to support any curve which implements
+//! algebra's `PairingEngine` trait. We will also support public keys on G1 and signatures on G2.
 
 pub(crate) mod bls;
 pub use bls::{PrivateKey, PublicKey, PublicKeyCache, Signature};
