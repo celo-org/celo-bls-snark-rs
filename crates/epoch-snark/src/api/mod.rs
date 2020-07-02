f5addeecd7b40af584344c3dd799d22c87848de3
Georgios Konstantopoulos <me@gakonst.com>
2020-07-02 17:10:57 +0300

    feat: make curves & params types public (#178)

--- crates/epoch-snark/src/api/mod.rs
+++ crates/epoch-snark/src/api/mod.rs
@@ -2,14 +2,14 @@ mod prover;
 pub use prover::prove;
 
 mod setup;
-pub use setup::trusted_setup;
+pub use setup::{trusted_setup, Parameters};
 
 mod verifier;
 pub use verifier::{verify, VerificationError};
 
 // Instantiate certain types to avoid confusion
 use algebra::{bls12_377, bw6_761};
-type BLSCurve = bls12_377::Bls12_377;
+pub type BLSCurve = bls12_377::Bls12_377;
 type CPField = bw6_761::Fr;
-type CPCurve = bw6_761::BW6_761;
+pub type CPCurve = bw6_761::BW6_761;
 type CPFrParams = bw6_761::FrParameters;
