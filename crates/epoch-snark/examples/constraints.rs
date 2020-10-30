7f45af4e8070bcc403cb057d2fe616db0e63ce9f
Michael Straka <mstraka100@gmail.com>
2020-10-29 17:19:04 -0700

    modified authors list

--- crates/epoch-snark/examples/constraints.rs
+++ crates/epoch-snark/examples/constraints.rs
@@ -1,10 +1,10 @@
-/*use algebra::bls12_377::Bls12_377;
+use algebra::bls12_377::Bls12_377;
 use epoch_snark::ValidatorSetUpdate;
-use r1cs_core::ConstraintSynthesizer;
-use std::env;*/
+use r1cs_core::{ConstraintSystem, ConstraintSynthesizer};
+use std::env;
 
 fn main() {
-/*    let mut args = env::args();
+    let mut args = env::args();
     args.next().unwrap(); // discard the program name
     let num_validators = args
         .next()
@@ -18,9 +18,9 @@ fn main() {
         .expect("NaN");
     let faults = (num_validators - 1) / 3;
 
-    let mut cs = ConstraintCounter::new();
+    let cs = ConstraintSystem::new_ref();
     let circuit = ValidatorSetUpdate::<Bls12_377>::empty(num_validators, num_epochs, faults, None);
-    circuit.generate_constraints().unwrap();
+    circuit.generate_constraints(cs.clone()).unwrap();
 
     println!(
         "Number of constraints for {} epochs ({} validators, {} faults, hashes in BW6_761): {}",
@@ -28,6 +28,5 @@ fn main() {
         num_validators,
         faults,
         cs.num_constraints()
-    )*/
-    println!("Hello world!");
+    )
 }
