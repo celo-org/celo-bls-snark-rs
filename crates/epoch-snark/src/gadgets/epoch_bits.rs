78dc35077eec094847319242f5d6deeb9ddfd49b
Kobi Gurkan <kobigurk@gmail.com>
2020-11-10 08:51:54 +0200

    remove some reversals

--- crates/epoch-snark/src/gadgets/epoch_bits.rs
+++ crates/epoch-snark/src/gadgets/epoch_bits.rs
@@ -61,7 +61,6 @@ impl EpochBits {
         let first_and_last_bits = [self.first_epoch_bits.clone(), self.last_epoch_bits.clone()];
         for bits in first_and_last_bits.iter() {
             let mut message = bits.to_owned();
-            message.reverse();
             let message_rounded_len = 8 * ((message.len() + 7) / 8);
             message.resize(message_rounded_len, Bool::constant(false));
 
@@ -147,7 +146,7 @@ fn le_chunks(iter: &[Bool], chunk_size: u32) -> Vec<Vec<Bool>> {
 mod tests {
     use super::*;
     use crate::{epoch_block::hash_to_bits, gadgets::pack};
-    use bls_gadgets::utils::{bytes_le_to_bits_be, test_helpers::print_unsatisfied_constraints};
+    use bls_gadgets::utils::{test_helpers::print_unsatisfied_constraints, bytes_le_to_bits_le};
 
     use r1cs_core::ConstraintSystem;
     use rand::RngCore;
@@ -168,8 +167,8 @@ mod tests {
 
         let cs = ConstraintSystem::<Fr>::new_ref();
         // encode each epoch's bytes to LE and pass them to the constraint system
-        let first_epoch_bits = bytes_le_to_bits_be(&first_bytes, 256);
-        let last_epoch_bits = bytes_le_to_bits_be(&last_bytes, 256);
+        let first_epoch_bits = bytes_le_to_bits_le(&first_bytes, 256);
+        let last_epoch_bits = bytes_le_to_bits_le(&last_bytes, 256);
         let bits = EpochBits {
             crh_bits: vec![],
             xof_bits: vec![],
