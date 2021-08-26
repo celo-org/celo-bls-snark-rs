5e7362d74b48fcf9987f6379d8b300fd020e98cd
George Tankersley <george.tankersley@gmail.com>
2021-08-25 17:58:29 -0700

    batch: add benchmark for strict batching

--- crates/bls-crypto/benches/batch_bls.rs
+++ crates/bls-crypto/benches/batch_bls.rs
@@ -5,6 +5,7 @@ use ark_bls12_377::{G1Projective, G2Projective};
 use ark_ff::Zero;
 
 use bls_crypto::{
+    bls::Batch,
     hash_to_curve::try_and_increment_cip22::COMPOSITE_HASH_TO_G1_CIP22, PrivateKey, PublicKey,
     Signature, SIG_DOMAIN,
 };
@@ -35,25 +36,31 @@ fn batch_bls_comparison(c: &mut Criterion) {
     let mut asig = G1Projective::zero();
     let mut pubkeys = Vec::new();
     let mut sigs = Vec::new();
+    let mut epoch_batches = Vec::new();
+
     for msg in msgs.iter().take(NUM_BLOCKS) {
         let mut epoch_pubkey = G2Projective::zero();
         let mut epoch_sig = G1Projective::zero();
+        let mut epoch_batch = Batch::new(msg.0, msg.1);
         for _ in 0..NUM_VALIDATORS {
             let sk = PrivateKey::generate(rng);
             let s = sk.sign(msg.0, msg.1, try_and_increment).unwrap();
 
             epoch_sig += s.as_ref();
             epoch_pubkey += sk.to_public().as_ref();
+            epoch_batch.add(sk.to_public(), s);
         }
 
         pubkeys.push(PublicKey::from(epoch_pubkey));
         sigs.push(Signature::from(epoch_sig));
 
         asig += epoch_sig;
+
+        epoch_batches.push(epoch_batch);
     }
 
     // verify the sigs individually in a loop
-    group.bench_function("individual verification", |b| {
+    group.bench_function("individual aggregate verification", |b| {
         b.iter(|| {
             pubkeys
                 .iter()
@@ -66,10 +73,18 @@ fn batch_bls_comparison(c: &mut Criterion) {
     });
 
     let asig = Signature::from(asig);
-    group.bench_function("batch verification", |b| {
+    group.bench_function("batch aggregate verification", |b| {
         b.iter(|| {
             asig.batch_verify(&pubkeys, SIG_DOMAIN, &msgs, try_and_increment)
                 .unwrap()
+        })
+    });
+
+    group.bench_function("per-epoch batch verification", |b| {
+        b.iter(|| {
+            epoch_batches.iter().for_each(|batch| {
+                batch.verify(try_and_increment).unwrap()
+            })
         })
     });
 }
