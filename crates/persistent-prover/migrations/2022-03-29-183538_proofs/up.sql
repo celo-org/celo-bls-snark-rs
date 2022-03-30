5c86a89ef14c50cf1e775818558e3e12acd0613a
Kobi Gurkan <kobigurk@gmail.com>
2022-03-30 23:03:21 +0300

    fix: fix table schema

--- crates/persistent-prover/migrations/2022-03-29-183538_proofs/up.sql
+++ crates/persistent-prover/migrations/2022-03-29-183538_proofs/up.sql
@@ -1,6 +1,8 @@
 CREATE TABLE proofs (
     id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    first_epoch_index INTEGER NOT NULL,
     first_epoch BLOB NOT NULL,
+    last_epoch_index INTEGER NOT NULL,
     last_epoch BLOB NOT NULL,
     proof BLOB NOT NULL
 )
--- crates/persistent-prover/src/schema.rs
+++ crates/persistent-prover/src/schema.rs
@@ -1,7 +1,9 @@
 table! {
     proofs (id) {
         id -> Integer,
+        first_epoch_index -> Integer,
         first_epoch -> Binary,
+        last_epoch_index -> Integer,
         last_epoch -> Binary,
         proof -> Binary,
     }
